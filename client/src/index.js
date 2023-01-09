3ff690c2ace20bfb7cc0e3e9476d43ee56f07a8f
Марина <mp795610@gmail.com>
2023-01-09 10:32:52 +0500

    Connection attempt

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from './App';
-import Guk from './guk/guk';
-import Irit from './irit-rtf/irit-rtf';
-
+import App from './App.js';
+  
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/irit" element={<Irit />} />
-      <Route path="/guk" element={<Guk />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>
 );
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import App from './App';
+// import Guk from './guk/guk.js';
+// import Irit from './irit-rtf/irit-rtf.js';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<App />} />
+//       <Route path="/irit" element={<Irit />} />
+//       <Route path="/guk" element={<Guk />} />
+//     </Routes>
+//   </BrowserRouter>
+// );
--- client/src/irit-rtf/irit-rtf.js
+++ client/src/irit-rtf/irit-rtf.js
@@ -1,22 +1,26 @@
 import React from "react";
-/*import { getShortestPath } from '../utils/paths.js'*/
+import { getShortestPath } from '../utils/paths.js'
 import './style.css';
 import zeroFloor from '../img/ИРИТ-РТФ-0-этаж.jpg'
 import firstFloor from '../img/ИРИТ-РТФ-1-этаж.jpg'
 import secondFloor from '../img/ИРИТ-РТФ-2-этаж.jpg'
 import thirdFloor from '../img/ИРИТ-РТФ-3-этаж.jpg'
 import forhtFloor from '../img/ИРИТ-РТФ-4-этаж.jpg'
+import loope from '../img/loupe.svg';
+
 
 function ShowIrit() {
   return (
     <div className="container">
         <div className="search flex">
-            <div className="searc-string flex">
-                <input className="input-reset searc-string-input" type="text" name="input-point" id="" placeholder="Аудитория или название места"/>
+            <div className="search-field flex">
+                <span className="search-field-text">От</span><input className="input-reset search-string-input from-point" type="text"/>
+                <span className="search-field-text">До</span><input className="input-reset search-string-input to-point" type="text"/>
+                <button className="btn-reset loope-btn" onClick={ search }><img className="loope" src={ loope } alt=""/></button>
             </div>
         </div>
         <div className="div">
-            <ul className="buttons-floors list-reset">
+            <ul className="buttons-floors list-reset" onClick={ changeMap }>
                 <p className="ins-name">PТФ</p>
                 <li className="floor"><button className="button-floor btn-reset" value="4">4</button></li>
                 <li className="floor"><button className="button-floor btn-reset" value="3">3</button></li>
@@ -30,10 +34,18 @@ function ShowIrit() {
   );
 };
 
-var floors = document.querySelector('.buttons-floors');
-var mapOnScreen = document.querySelector('.img-map');
+async function search() {
+  console.log(await getShortestPath("ИРИТ-РТФ", "Р-407", "Р-402а"));
 
-/*console.log(getShortestPath("ИРИТ-РТФ", "Р-407", "Р-402а"));*/
+  var pointFrom = document.querySelector('.from-point').value;
+  var pointTo = document.querySelector('.to-point').value;
+
+  console.log(pointFrom);
+  console.log(pointTo);
+  console.log(await getShortestPath("ИРИТ-РТФ", "pointFrom",  "pointTo"));
+}
+
+
 
 var maps = {
   4 : forhtFloor,
@@ -43,82 +55,28 @@ var maps = {
   0 : zeroFloor
 };
 
-if (mapOnScreen) {
-  floors.addEventListener('click', (evt) => {
-    const mapBtn = evt.target.closest('.button-floor');
+function changeMap(evt) {
+  const mapBtn = evt.target.closest('.button-floor');
+  var floorsBtns = document.querySelector('.buttons-floors');
+  var mapOnScreen = document.querySelector('.img-map');
 
-    if (mapBtn) {
-      floors.querySelectorAll('.button-floor').forEach((f) => f.classList.remove('button-floor-active'));
+  if (mapBtn) {
+    floorsBtns.querySelectorAll('.button-floor').forEach((f) => f.classList.remove('button-floor-active'));
 
-      if (mapBtn.value === '4') {
-        mapOnScreen.src = maps[4];
-      } else if (mapBtn.value === '3') {
-        mapOnScreen.src = maps[3];
-      } else if (mapBtn.value === '2') {
-        mapOnScreen.src = maps[2];
-      } else if (mapBtn.value === '1') {
-        mapOnScreen.src = maps[1];
-      } else {
-        mapOnScreen.src = maps[0];
-      }
-
-      mapBtn.classList.add('button-floor-active');
+    if (mapBtn.value === '4') {
+      document.querySelector('.img-map').src = maps[4];
+    } else if (mapBtn.value === '3') {
+      mapOnScreen.src = maps[3];
+    } else if (mapBtn.value === '2') {
+      mapOnScreen.src = maps[2];
+    } else if (mapBtn.value === '1') {
+      mapOnScreen.src = maps[1];
+    } else {
+      mapOnScreen.src = maps[0];
     }
-  })
+
+    mapBtn.classList.add('button-floor-active');
+  }
 }
-
-
-
-// var floors = document.querySelectorAll('.buttons-floor');
-
-// var addThumbnailClickHandler = function (floor, map) {
-//   floor.addEventListener('click', function () {
-//     floors.forEach((f) => f.classList.remove('button-floor-active'));
-//     floor.classList.add('button-floor-active');
-//     mapOnScreen.src = map;
-
-//       // mapOnScreen.src = new Indoor.Map(this._canv, {
-//       //   floorplan: new Indoor.Floor({
-//       //     url: map,
-//       //     width: this.mapImg.width,
-//       //     height: this.mapImg.height  2,
-//       //     opacity: 0.8,
-//       //     zIndex: 1
-//       //   }),
-//       //   height: window.innerHeight,
-//       //   minZoom: 0.1,
-//       //   maxZoom: 100,
-//       //   center: {
-//       //     x: 0,
-//       //     y: 0
-//       //   }
-//       // })
-//   });
-// };
-
-// for (var i = 0; i < floors.length; i++) {
-//   addThumbnailClickHandler(floors[i], maps[floors[i].value]);
-// }
-
-
-
-// const mapEl = document.querySelector('.container');
-// const Indoor = require('indoorjs')
- 
-// const map = new Indoor.Map(mapEl, {
-//   floorplan: new Indoor.Floor({
-//     url: './img/ИРИТ-РТФ-1-этаж.jpg',
-//     opacity: 0.4,
-//     width: 400,
-//     zIndex: 1
-//   }),
-//   minZoom: 0.001,
-//   maxZoom: 10,
-//   center: {
-//     x: 0,
-//     y: 0,
-//     zoom: 1
-//   }
-// });
 
 export default ShowIrit;
